7d1967a31cdf7dc5bb850a94514ac0bfe6bf5885
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2024-09-22 02:15:42 +0530

    remove toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 import PortfolioContainer from './Component/PortfolioContainer/PortfolioContainer';
 import Dashboard from './Screens/Dashboard/Dashboard';
@@ -15,7 +13,6 @@ const App = () => {
         <Route path="/" element={<PortfolioContainer />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-      <ToastContainer />
     </Router>
    
   );
